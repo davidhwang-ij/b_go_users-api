725de242b7a0ce2fe87ffccf552054a906d0d269
David Hwang <davidhwang.ij@gmail.com>
2021-01-08 22:21:15 -0500

    Added CreateUser, GetUser and mock data

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/davidhwang-ij/bookstore_users-api/domain/users"
 	"github.com/davidhwang-ij/bookstore_users-api/services"
@@ -28,7 +29,21 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+
 }
 
 func SearchUser(c *gin.Context) {
