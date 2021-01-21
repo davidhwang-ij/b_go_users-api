cc05bf6ad5e01040d2f39b5cc24115eebd5a6867
David Hwang <davidhwang.ij@gmail.com>
2021-01-20 22:24:16 -0500

    Marshalling user for public & private requests

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -33,7 +33,7 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, result)
+	c.JSON(http.StatusCreated, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Get(c *gin.Context) {
@@ -49,7 +49,7 @@ func Get(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user.Marshall(c.GetHeader("X-Public") == "true"))
 
 }
 
@@ -76,7 +76,7 @@ func Update(c *gin.Context) {
 		c.JSON(err.Status, err)
 	}
 
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Delete(c *gin.Context) {
@@ -102,5 +102,6 @@ func Search(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, users)
+
+	c.JSON(http.StatusOK, users.Marshall(c.GetHeader("X-Public") == "true"))
 }
